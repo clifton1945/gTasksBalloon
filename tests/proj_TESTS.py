eb3e5babae7eb88d946eec6d96a126d373afc9ce
clif edwards <clif.edwards.2@gmail.com>
2014-03-18 10:29:22 -0400

    GIW  WORKING proj_tests.py    UPDATED AND GIW stable 9//1 skip will tag ver_4.8

--- tests/proj_TESTS.py
+++ tests/proj_TESTS.py
@@ -4,8 +4,7 @@ __author__ = 'CLIF'
 
 import unittest
 import server_TESTS
-# import task_model_TESTS
-import task_model_server_tests
+import tlt_object_tests
 
 
 class ProjectTests(unittest.TestCase):
@@ -15,9 +14,8 @@ class ProjectTests(unittest.TestCase):
 loader = unittest.TestLoader()
 
 suite = loader.loadTestsFromModule(server_TESTS)
-# suite.addTests(loader.loadTestsFromModule(task_model_TESTS))
+suite.addTests(loader.loadTestsFromModule(tlt_object_tests))
 suite.addTests(loader.loadTestsFromModule(presenter_TESTS))
-suite.addTests(loader.loadTestsFromModule(task_model_server_tests))
 
 runner = unittest.TextTestRunner(verbosity=1)
 result = runner.run(suite)
